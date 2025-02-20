b1ad0edc54365705b233f68d88f6ad91f361c2d6
Natu1108 <nturner815@gmail.com>
2025-02-20 15:25:15 -0600

    idk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ const executeCode = async (code, language) => {
   outputElement.textContent = 'Executing...';
 
   try {
-    const repsponse = await fetch('https://localhost:5000/execute', {
+    const repsponse = await fetch('https://ReplitCopy.onrender.com/execute', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ code, language }),
